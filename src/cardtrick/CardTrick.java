432a2de46d587a4777b5bf34f582ce6ec86d5c89
The Hai Nguyen <nguytheh@sheridancollege.ca>
2024-09-18 13:53:05 -0400

    Complete the CardTrick class to implement the magicHand

--- src/cardtrick/CardTrick.java
+++ src/cardtrick/CardTrick.java
@@ -3,6 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package cardtrick;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -20,7 +24,16 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            Random rand = new Random();
+            int v = rand.nextInt(13) + 1;
+            c.setValue(v);
+
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            int s = rand.nextInt(4);
+            c.setSuit(Card.SUITS[s]);
+            
+            //Add card into card array
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
