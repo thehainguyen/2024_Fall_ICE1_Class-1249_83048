258675f77ea959a0b5d5a236fdbeab1be2f5ff8c
The Hai Nguyen <nguytheh@sheridancollege.ca>
2024-09-18 14:20:22 -0400

    Implement user input part

--- src/cardtrick/CardTrick.java
+++ src/cardtrick/CardTrick.java
@@ -3,16 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package cardtrick;
-import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Set;
+
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Nagma
+ * @modified by The Hai Nguyen - 991745555
  */
 public class CardTrick {
     
@@ -39,6 +39,29 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Pick a card value (1-13): ");
+        int inputValue = scanner.nextInt();
+        
+        System.out.print("Pick a card suit (Hearts, Diamonds, Spades, Clubs): ");
+        String inputSuit = scanner.next();
+        
+        Card inputCard = new Card();
+        inputCard.setValue(inputValue);
+        inputCard.setSuit(inputSuit);
+               
+        boolean isMagicCard = false;
+        for (Card c : magicHand) {
+            if (c.getValue() == inputValue && c.getSuit().equals(inputSuit)){
+                System.out.println("Your card is in the magic hand");
+                isMagicCard = true;
+                break;
+            }
+        }
+        
+        if (!isMagicCard) {
+            System.out.println("Your card is not in the magic hand");
+        }
     }
     
 }
